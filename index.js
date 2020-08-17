0a0272c7b73d0e8d8b70ba7557cee8bc87682ef0
Abdulateef Al-radaee <abdulateef.radaee@gmail.com>
2020-08-17 15:01:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ function testFunc() {
   return "hi"
 }
 
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]
+
 function superbowlWin(array) {
   return array.find( e => e.result === 'W'? e.year: undefined)
 }
